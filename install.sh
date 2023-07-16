c278fce0a3d2d2ea043dc4a97efe42e656357221
AJPNetworks <alexander@ajp.network>
2023-07-15 21:03:11 -0400

    Bug fix (fr this time)
    I should've used return 1 instead of exit to mitigate the previous bug in last commit where the system would log you back out if a file was missing

--- install.sh
+++ install.sh
@@ -109,9 +109,9 @@ echo ""
 if ! echo '#!/bin/bash
 profile_dir=$(find ~/.mozilla/firefox/ -name '*.default-esr' -type d)
 if [ -z "$profile_dir" ]; then
-    sleep 5
+    echo ""
     echo "Firefox profile directory not found."
-    exit 1
+    return 1
 fi
 prefsfile="$profile_dir/sessionstore-backups"
 
